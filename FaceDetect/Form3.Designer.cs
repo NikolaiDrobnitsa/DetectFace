6d4d4f9f224dbb5245c88024c6386289f0e5e4d3
Nikolai <Drob_mf41@student.itstep.org>
2023-03-18 23:06:18 +0200

    Добавьте файлы проекта.

---
+++ FaceDetect/Form3.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace FaceDetect
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button3);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.listBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1122, 547);
+            this.splitContainer1.SplitterDistance = 196;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.Location = new System.Drawing.Point(0, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Upload to S3";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Upload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(2, 70);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(195, 469);
+            this.listBox1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(922, 547);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.Location = new System.Drawing.Point(0, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(196, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Show";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1122, 547);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private Button button2;
+        private Button button1;
+        private ListBox listBox1;
+        private PictureBox pictureBox1;
+        private Button button3;
+    }
+}
---
+++ FaceDetect/Form3.cs
@@ -0,0 +1,214 @@
+﻿using Amazon.S3.Transfer;
+using Amazon.S3;
+using System;
+using System.Collections.Generic;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using Amazon.S3.Model;
+using Amazon;
+using Amazon.Rekognition.Model;
+using Amazon.Rekognition;
+using System.Drawing.Imaging;
+using Image = System.Drawing.Image;
+
+namespace FaceDetect
+{
+    public partial class Form3 : Form
+    {
+        //private Button button1;
+        //private Button button2;
+        //private PictureBox pictureBox1;
+
+        public Form3()
+        {
+            InitializeComponent();
+            LoadS3FilesToListBox();
+            //InitializeControls();
+        }
+        private void InitializeControls()
+        {
+
+            //button1 = new Button
+            //{
+            //    Text = "Upload",
+            //    Dock = DockStyle.Top
+            //};
+            //button1.Click += button1_Click;
+            //Controls.Add(button1);
+            //button2 = new Button
+            //{
+            //    Text = "Upload to S3",
+            //    Dock = DockStyle.Top
+            //};
+            //button2.Click += button2_Click;
+            //Controls.Add(button2);
+
+            // Create a picture box to display the image with detected faces
+            //pictureBox1 = new PictureBox
+            //{
+            //    SizeMode = PictureBoxSizeMode.Zoom,
+            //    Dock = DockStyle.Fill
+            //};
+            //Controls.Add(pictureBox1);
+        }
+        private static readonly string bucketName = "nikolyabucket";
+        private static string keyName = "imgface.png";
+        private static readonly string accessKey = "AKIAV5NLL37Y36O5Q43I";
+        private static readonly string secretKey = "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";
+        private static readonly RegionEndpoint regionEndpoint = RegionEndpoint.USEast2;
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = fileDialog.FileName;
+                await UploadToS3Async(filePath);
+            }
+        }
+        private async void LoadS3FilesToListBox()
+        {
+            listBox1.Items.Clear();
+            var s3Client = new AmazonS3Client(accessKey, secretKey, regionEndpoint);
+
+            var listObjectsRequest = new ListObjectsRequest
+            {
+                BucketName = bucketName
+            };
+
+            var listObjectsResponse = await s3Client.ListObjectsAsync(listObjectsRequest);
+
+            foreach (var s3Object in listObjectsResponse.S3Objects)
+            {
+                listBox1.Items.Add(s3Object.Key);
+            }
+        }
+        public static async Task<Stream> DownloadFromS3Async()
+        {
+            try
+            {
+                var client = new AmazonS3Client(accessKey, secretKey, regionEndpoint);
+                var request = new GetObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = keyName
+                };
+                var response = await client.GetObjectAsync(request);
+                return response.ResponseStream;
+            }
+            catch (AmazonS3Exception ex)
+            {
+                Console.WriteLine($"Error encountered ***. Message:'{ex.Message}' when reading an object");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unknown error encountered on server. Message:'{ex.Message}' when reading an object");
+            }
+            return null;
+        }
+        //private async void button1_Click(object sender, EventArgs e)
+        //{
+        //    var imageStream = await DownloadFromS3Async();
+        //    if (imageStream != null)
+        //    {
+        //        pictureBox1.Image =  Image.FromStream(imageStream);
+        //    }
+        //}
+        private async Task UploadToS3Async(string filePath)
+        {
+            try
+            {
+                var client = new AmazonS3Client(accessKey, secretKey, regionEndpoint);
+                var putRequest = new PutObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = Path.GetFileName(filePath),
+                    FilePath = filePath
+                };
+                var response = await client.PutObjectAsync(putRequest);
+                //MessageBox.Show($"Upload completed. Request Id: {response.ResponseMetadata.RequestId}");
+                LoadS3FilesToListBox();
+            }
+            catch (AmazonS3Exception ex)
+            {
+                MessageBox.Show($"Error encountered ***. Message:'{ex.Message}' when uploading an object");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Unknown error encountered on server. Message:'{ex.Message}' when uploading an object");
+            }
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            show_imgae();
+        }
+        public async void show_imgae()
+        {
+            var imageStream = await DownloadFromS3Async();
+            if (imageStream != null)
+            {
+                AmazonRekognitionClient rekognitionClient = new AmazonRekognitionClient(accessKey, secretKey, regionEndpoint);
+
+                MemoryStream memoryStream = new MemoryStream();
+                imageStream.CopyTo(memoryStream);
+                byte[] imageBytes = memoryStream.ToArray();
+                Image image = Image.FromStream(memoryStream);
+
+                DetectFacesRequest detectFacesRequest = new DetectFacesRequest()
+                {
+                    Image = new Amazon.Rekognition.Model.Image()
+                    {
+                        Bytes = new MemoryStream(imageBytes)
+                    },
+                    Attributes = new List<String>()
+                    {
+                "ALL"
+                    }
+                };
+
+                DetectFacesResponse detectFacesResponse = await rekognitionClient.DetectFacesAsync(detectFacesRequest);
+
+                if (detectFacesResponse.FaceDetails.Count > 0)
+                {
+                    // Create a Bitmap object from the original image
+                    Bitmap bmp = new Bitmap(image);
+
+                    // Draw white rectangles around detected faces
+                    using (Graphics graphics = Graphics.FromImage(bmp))
+                    {
+                        Pen pen = new Pen(Color.White, 3);
+                        foreach (FaceDetail faceDetail in detectFacesResponse.FaceDetails)
+                        {
+                            graphics.DrawRectangle(pen, faceDetail.BoundingBox.Left * bmp.Width, faceDetail.BoundingBox.Top * bmp.Height, faceDetail.BoundingBox.Width * bmp.Width, faceDetail.BoundingBox.Height * bmp.Height);
+                        }
+                    }
+
+                    // Display the modified image in the picture box
+                    pictureBox1.Image = bmp;
+                    //MessageBox.Show("if");
+
+                }
+                else
+                {
+                    // If no faces are detected, display the original image
+                    pictureBox1.Image = image;
+                    //MessageBox.Show("else");
+
+                }
+            }
+        } 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex > -1)
+            {
+                keyName = listBox1.SelectedItem.ToString();
+                MessageBox.Show("item =" + listBox1.SelectedItem + "\nkeyname: " + keyName + "\nindex:" + listBox1.SelectedIndex);
+                show_imgae();
+            }
+
+        }
+    }
+}
